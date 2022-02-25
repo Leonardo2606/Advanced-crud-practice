44be78d9a00d35985b36d52aa94f0065f8cb59ab
Leonardo Rebouças da Silva <leonardo.777@live.com>
2022-02-25 13:48:57 -0300

    Importando provider de react-redux e usando-o com a store também importada.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import {
 import Menu from "./components/Menu";
 import Cadastro from './components/Cadastro';
 import Lista from "./components/Lista";
+import {Provider} from 'react-redux';
+import store from './redux/store.js';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Menu />}/>
-        <Route path='/Cadastro' element={<Cadastro />}/>
-        <Route path='/Lista' element={<Lista />}/>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Menu />}/>
+          <Route path='/Cadastro' element={<Cadastro />}/>
+          <Route path='/Lista' element={<Lista />}/>
+        </Routes>
+      </Router>   
+    </Provider>
+     
   );
 }
 
