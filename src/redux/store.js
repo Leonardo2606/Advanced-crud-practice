484779a637871605f9e8e32fa3dd48fc92f4d932
Leonardo Rebouças da Silva <leonardo.777@live.com>
2022-03-03 13:15:21 -0300

    Nomes traduzidos para o inglês

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import empresasReducer from "./empresasReducer";
+import companiesReducer from "./companiesReducer";
 
 const store = configureStore({
     reducer: {
-        empresas: empresasReducer
+        companies: companiesReducer
     }
 })
 
