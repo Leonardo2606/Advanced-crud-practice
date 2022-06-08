33af7302abc3727a59df7cc4e1a73c46c2027283
Leonardo Rebouças <leonardo.777@live.com>
2022-06-08 17:42:15 -0300

    Transferindo axios e as urls das apis utilizadas do arquivo deletado api.js para o novo hook useMaskAndApi, também foram retirado as funções mascara de cnpj e cep assim como a função de request da api do cep e um estado que guarda as informações

---
+++ src/custom_hooks/useMaskAndApi.js
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+
+function useMaskAndApi() {
+    
+    const UnidadeFederalApi = axios.create({
+       baseURL: 'https://servicodados.ibge.gov.br/api/v1/localidades/'
+    });
+    const cepApi = axios.create({
+       baseURL: 'https://viacep.com.br/ws'
+    });
+    
+    const [federalUnits, setUF] = useState([]);
+    useEffect(()=>{
+        UnidadeFederalApi.get('estados')
+        .then(response=>{setUF(response.data)})
+        .catch(error=>console.log(error))
+    }, [])
+    
+    const [requestedCep, setRequestedCep] = useState({});
+    async function requestCep(cepEvent) {
+        const cep = cepEvent.target.value;
+        try{
+            if(cep.length === 9) {
+                const response = await cepApi.get(`/${cep}/json`);
+                setRequestedCep(response.data);
+            }
+        } catch(err){
+            console.log(err);
+        } finally {
+            return cep
+        }
+    }
+
+    function cnpjMask(cnpj) {
+        if (cnpj.length <= 18) {  
+            cnpj = cnpj.replace(/\D/g, '');
+            cnpj = cnpj.replace(/^(\d{2})(\d)/, "$1.$2");
+            cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+            cnpj = cnpj.replace(/\.(\d{3})(\d)/, ".$1/$2");
+            cnpj = cnpj.replace(/(\d{4})(\d)/,"$1-$2");
+        } cnpj = cnpj.substring(0,18);
+        return cnpj;
+    }
+
+    function cepMask(cep) {
+        if(cep.length <= 9) {
+            cep = cep.replace(/\D/g, '')
+            cep = cep.replace(/^(\d{5})(\d)/, "$1-$2")
+        } cep = cep.substring(0,9)
+        return cep;
+    }
+
+    return [federalUnits, cnpjMask, cepMask, requestCep, requestedCep];
+
+}
+
+export default useMaskAndApi;
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const UnidadeFederalApi = axios.create({
-    baseURL: 'https://servicodados.ibge.gov.br/api/v1/localidades/'
-});
-
-export const cepApi = axios.create({
-    baseURL: 'https://viacep.com.br/ws'
-});
