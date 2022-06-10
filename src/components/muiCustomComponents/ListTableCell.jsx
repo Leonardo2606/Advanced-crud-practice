279678d4791d5cc98422a37f2c978d04cc01ec1a
Leonardo Rebouças <leonardo.777@live.com>
2022-06-10 17:53:14 -0300

    Pasta criada para guardar componentes materialUI customizados.

---
+++ src/components/muiCustomComponents/ListTableCell.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { TableCell } from '@mui/material';
+import { alpha, styled } from '@mui/material/styles';
+
+const CustomTableCell = styled(TableCell)(() => ({
+    padding: 5,
+    paddingLeft: 16,
+    margin: 'auto !important'
+}))
+
+export default function ListTableCell({ID, value}){
+    return <CustomTableCell id={ID}>{value}</CustomTableCell>
+}
